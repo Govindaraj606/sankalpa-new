d4747049428ffdc2c3289f6497428830cf4d12b3
Govindaraj606 <govindarajk8092@gmail.com>
2025-06-09 07:48:03 -0400

    add function

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Homepage from './Homepage/Homepage'
 import Footer from './footer/Footer'
 import Navbar from './nav-bar/Navbar'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
 import Sridutt from './pages/Sridutt/Sridutt'
 import Jagadish from './pages/Jagadish/Jagadish'
 import Contact from './Contact/Contact'
@@ -12,19 +12,27 @@ import Entpart from './ent-part/ENTpart'
 
 
 const App = () => {
+
+  const pathname = useLocation()
+
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    })
+  }), [pathname]
+
   return (
     <div>
-      <BrowserRouter>
         <Navbar />
         <Routes>
           <Route path='/' element={<Homepage />} />
           <Route path='/doctor/dr-jagadish' element={<Jagadish />} />
-          <Route path='/doctor/dr-sridutt' element={<Sridutt/>}/>
+          <Route path='/doctor/dr-sridutt' element={<Sridutt />} />
           <Route path='/Contact' element={<Contact />} />
 
         </Routes>
         <Footer />
-      </BrowserRouter>
 
     </div>
   )
